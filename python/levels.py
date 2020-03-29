46277c1559d84287015d45da8342381d767e06d1
Neamar <neamar@neamar.fr>
2020-03-29 11:57:46 +0200

    Proper formatting, now we can start looking for insteresting puzzles
    1   T          0          Level solution: 1
    2   TTT∀       0000       Level solution: 4
    3   T↕TT↕T     000000     Level solution: 25
    4   T↕TT∀T     111001     Level solution: 256
    5   ∀↕↕T       1001       Level solution: 34
    6   T↕↕↕T      10001      Level solution: 3
    7   T↕↕↕T      10001      Level solution: 3

--- python/levels.py
+++ python/levels.py
@@ -10,9 +10,9 @@ levels = (
   ('T↕↕↕T', '10001'),
 )
 
-for level, initial_state in levels:
+for index, (level, initial_state) in enumerate(levels):
   if len(level) != len(initial_state):
     raise Exception("State and level must have the same length")
 
   solution = solve_level(level, initial_state)
-  print("%s %s Level solution: %s" % (level.ljust(10), initial_state.ljust(10), solution))
+  print("%s %s %s Level solution: %s" % (str(index + 1).ljust(3), level.ljust(10), initial_state.ljust(10), solution))
