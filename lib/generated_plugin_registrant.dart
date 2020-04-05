a1c325414da40b0f433980bbf898d63269011afa
Neamar <neamar@neamar.fr>
2020-04-05 20:32:32 +0200

    Moved back to beta channel

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,7 +10,6 @@ import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
